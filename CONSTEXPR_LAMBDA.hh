2de5d223d1231506607dad6fa59494b896dcbabd
Aaron McDaid <aaronmcdaid@gmail.com>
2017-12-25 21:52:22 +0000

    Some comments

--- CONSTEXPR_LAMBDA.hh
+++ CONSTEXPR_LAMBDA.hh
@@ -1,16 +1,18 @@
+/* CONSTEXPR_LAMBDA - a macro to allow constexpr lambdas in C++14.
+ *
+ * More info, including a full explanation: https://github.com/aaronmcdaid/CONSTEXPR_LAMBDA
+ *
+ * Aaron McDaid - aaron.mcdaid@gmail.com - https://aaronmcdaid.github.io/
+ */
 #include <type_traits>
 #include <utility>
 
 namespace CONSTEXPR_LAMBDA_namespace
 {
-    template<typename L>
-    auto
-    null_address_of(L &&)
-    ->std::decay_t<L>*
-    {
-        return nullptr;
-    }
-
+    /*
+     * 'call_forwarder' has the call operator that is exposed to the outside,
+     * capturing the arguments and forwarding them through to the call operator
+     * in the inner class. */
     template<typename L>
     struct call_forwarder
     {
@@ -26,20 +28,46 @@ namespace CONSTEXPR_LAMBDA_namespace
 
     };
 
+    /* 'make_CONSTEXPR_LAMBDA' constructs a 'call_forwarder' of the appropriate type */
     template<typename L>
     auto constexpr
     make_CONSTEXPR_LAMBDA(L*)
     -> call_forwarder<L>
     { return {}; }
+
+    /* 'null_address_of' doesn't really do anything, it's never really called
+     * as it appears on the 'wrong' side of a   ?:  expression.
+     *
+     *  constexpr auto * p = true?nullptr : null_address_of(foo());
+     *
+     * 'p' is a constant expression there. It's value is 'nullptr', which isn't
+     * interesting. But it's useful as it gives us the type of the return value
+     * of 'foo()' in contexts where we can't type 'decltype'.
+     */
+    template<typename L>
+    auto
+    null_address_of(L &&)
+    ->std::decay_t<L>*
+    {
+        return nullptr;
+    }
+
 }
 
 
+/* The next five macros are simply to count the number of arguments to the CONSTEXPR_LAMBDA
+ * macro and forward accordingly. i.e.
+ *  CONSTEXPR_LAMBDA(&,a)       => CONSTEXPR_LAMBDA_with_this_many_args_2(&,a)
+ *  CONSTEXPR_LAMBDA(&,a,&&,b)  => CONSTEXPR_LAMBDA_with_this_many_args_4(&,a,&&,b)
+ */
 #define CONSTEXPR_LAMBDA(...) CONSTEXPR_LAMBDA_select_based_on_arg_count(COUNT_MACRO_ARGS(__VA_ARGS__))(__VA_ARGS__)
 #define COUNT_MACRO_ARGS(...) GET_ARG_11( __VA_ARGS__, 10,9,8,7,6,5,4,3,2,1,I_CANNOT_SEE_ZERO_ARGS)
 #define GET_ARG_11(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,N,...) N
 #define CONSTEXPR_LAMBDA_select_based_on_arg_count(n) CONSTEXPR_LAMBDA_select_based_on_arg_count_impl(n)
 #define CONSTEXPR_LAMBDA_select_based_on_arg_count_impl(n) CONSTEXPR_LAMBDA_with_this_many_args_ ## n
 
+
+// Next are the main macros, one for each number of possible arguments:
 #define CONSTEXPR_LAMBDA_with_this_many_args_2(REF_OF_ARG0, NAME_OF_ARG0)                   \
             CONSTEXPR_LAMBDA_start_of_the_macro_stuff                                       \
                         (   auto                    REF_OF_ARG0 NAME_OF_ARG0                \
@@ -63,6 +91,7 @@ namespace CONSTEXPR_LAMBDA_namespace
             {   CONSTEXPR_LAMBDA_BODY_OF_THE_FUNCTION
 
 
+// Finally, three macros to avoid repetitive code in the previous few macros
 #define CONSTEXPR_LAMBDA_start_of_the_macro_stuff                                           \
             CONSTEXPR_LAMBDA_namespace::make_CONSTEXPR_LAMBDA(true ? nullptr :              \
             CONSTEXPR_LAMBDA_namespace::null_address_of([]
